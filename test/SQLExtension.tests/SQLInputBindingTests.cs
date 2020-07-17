e660f3f367dea03e0c06f14a5b321fc13a07690b
Sophia Tevosyan <t-sotevo@microsoft.com>
2020-07-17 16:21:45 -0700

    Forgot the string comparison thing

--- test/SQLExtension.tests/SQLInputBindingTests.cs
+++ test/SQLExtension.tests/SQLInputBindingTests.cs
@@ -402,8 +402,8 @@ namespace Microsoft.Azure.WebJobs.Extensions.SQL.Tests
                 {
                     return false;
                 }
-                return this.ID == otherData.ID && this.Cost == otherData.Cost && ((this.Name == null && otherData.Name == null) || this.Name.Equals(otherData.Name))
-                    && this.Timestamp.Equals(otherData.Timestamp);
+                return this.ID == otherData.ID && this.Cost == otherData.Cost && ((this.Name == null && otherData.Name == null) || 
+                    string.Equals(this.Name, otherData.Name, StringComparison.OrdinalIgnoreCase)) && this.Timestamp.Equals(otherData.Timestamp);
             }
         }
     }
