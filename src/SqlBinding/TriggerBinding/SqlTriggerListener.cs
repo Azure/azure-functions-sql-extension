e4a9629b27ebb60e5db432dd42f4de76abb29edf
Sophia Tevosyan <t-sotevo@microsoft.com>
2020-07-29 16:12:33 -0700

    first steps

---
+++ src/SqlBinding/TriggerBinding/SqlTriggerListener.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Azure.WebJobs.Host.Listeners;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Sql
+{
+    internal class SqlTriggerListener : IListener
+    {
+        SqlTableWatcher _watcher;
+
+        public void Cancel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _watcher = new SqlTableWatcher();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
