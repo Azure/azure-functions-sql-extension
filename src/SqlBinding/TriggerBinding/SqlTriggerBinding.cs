fa60e59f9c554058a216c7f51c6e8570c0774c9d
Sophia Tevosyan <t-sotevo@microsoft.com>
2020-07-31 16:49:52 -0700

    have the basic flow working

--- src/SqlBinding/TriggerBinding/SqlTriggerBinding.cs
+++ src/SqlBinding/TriggerBinding/SqlTriggerBinding.cs
@@ -2,8 +2,10 @@
 using Microsoft.Azure.WebJobs.Host.Listeners;
 using Microsoft.Azure.WebJobs.Host.Protocols;
 using Microsoft.Azure.WebJobs.Host.Triggers;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,13 +13,32 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
 {
     internal class SqlTriggerBinding : ITriggerBinding
     {
-        public Type TriggerValueType => throw new NotImplementedException();
+        private readonly string _connectionStringSetting;
+        private readonly string _table;
+        private readonly IConfiguration _configuration;
+        private readonly ParameterInfo _parameter;
+        private readonly IReadOnlyDictionary<string, Type> _emptyBindingContract = new Dictionary<string, Type>();
+        private readonly IReadOnlyDictionary<string, object> _emptyBindingData = new Dictionary<string, object>();
 
-        public IReadOnlyDictionary<string, Type> BindingDataContract => throw new NotImplementedException();
+        public SqlTriggerBinding(string table, string connectionStringSetting, IConfiguration configuration, ParameterInfo parameter)
+        {
+            _table = table;
+            _connectionStringSetting = connectionStringSetting;
+            _configuration = configuration;
+            _parameter = parameter;
+        }
+
+        public Type TriggerValueType => typeof(IEnumerable<SqlChangeTrackingEntry>);
+
+        public IReadOnlyDictionary<string, Type> BindingDataContract
+        {
+            get { return _emptyBindingContract; }
+        }
 
         public Task<ITriggerData> BindAsync(object value, ValueBindingContext context)
         {
-            throw new NotImplementedException();
+            // ValueProvider is via binding rules. 
+            return Task.FromResult<ITriggerData>(new TriggerData(null, _emptyBindingData));
         }
 
         public Task<IListener> CreateListenerAsync(ListenerFactoryContext context)
@@ -27,14 +48,17 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
                 throw new ArgumentNullException("context", "Missing listener context");
             }
 
-            return Task.FromResult<IListener>(new SqlTriggerListener(
-                context.Executor,
-                context.Descriptor.Id));
+            return Task.FromResult<IListener>(new SqlTriggerListener(_table, _connectionStringSetting, _configuration, context.Executor));
         }
 
         public ParameterDescriptor ToParameterDescriptor()
         {
-            throw new NotImplementedException();
+            return new SqlTriggerParameterDescriptor
+            {
+                Name = _parameter.Name,
+                Type = "SqlTrigger",
+                TableName = _table
+            };
         }
     }
 }
