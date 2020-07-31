fc90c0fed15b1f27c6992e1f36c268b43829b147
Sophia Tevosyan <t-sotevo@microsoft.com>
2020-07-30 22:01:11 -0700

    another comment update

--- src/SqlBinding/SqlAttribute.cs
+++ src/SqlBinding/SqlAttribute.cs
@@ -52,6 +52,9 @@ namespace Microsoft.Azure.WebJobs
         /// Specifies the parameters that will be used to execute the SQL query or stored procedure specified in <see cref="CommandText"/>. 
         /// Must follow the format "@param1=param1,@param2=param2". For example, if your SQL query looks like 
         /// "select * from Products where cost = @Cost and name = @Name", then Parameters must have the form "@Cost=100,@Name={Name}"
+        /// If the value of a parameter should be null, use "null", as in @param1=null,@param2=param2". 
+        /// If the value of a parameter should be an empty string, do not add anything after the equals sign and before the comma,
+        /// as in "@param1=,@param2=param2"
         /// </summary>
         [AutoResolve]
         public string Parameters { get; set; }
