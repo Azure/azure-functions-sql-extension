0e7c5d84b631cbca2fb056e77321cdc82f7981d6
Sophia Tevosyan <t-sotevo@microsoft.com>
2020-07-31 16:49:52 -0700

    have the basic flow working

--- src/SqlBinding/TriggerBinding/SqlChangeTrackingEntry.cs
+++ src/SqlBinding/TriggerBinding/SqlChangeTrackingEntry.cs
@@ -1,33 +1,50 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
-
-using System.IO;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Sql
 {
     /// <summary>
-    /// Represents a row that was changed in the user's table as well as metadata related to that change
-    /// </summary>
+    /// Represents a row that was changed in the user's table as well as metadata related to that change.
+    /// If the row was deleted, then <see cref="Data"/> is populated only with the primary key values of the deleted row
     /// <typeparam name="T">A user-defined POCO that represents a row of the table</typeparam>
     public class SqlChangeTrackingEntry<T>
     {
         /// <summary>
-        /// Specifies the type of change that occurred to the row. 
-        /// <see cref="WatcherChangeTypes.Changed"/> corresponds to an update, 
-        /// <see cref="WatcherChangeTypes.Created"/> corresponds to an insert, 
-        /// <see cref="WatcherChangeTypes.Deleted"/> corresponds to a delete
+        /// Initializes a new instance of the <see cref="SqlChangeTrackingEntry<typeparamref name="T"/>"/> class.
         /// </summary>
-        public WatcherChangeTypes ChangeType { get; set; }
+        /// <param name="changeType">
+        /// The type of change this entry corresponds to
+        /// </param>
+        /// <param name="data">
+        /// The current data in the user's table corresponding to the change (and only the primary key values 
+        /// of the row in the case that it was deleted)
+        /// </param>
+        public SqlChangeTrackingEntry(SqlChangeType changeType, T data)
+        {
+            ChangeType = changeType;
+            Data = data;
+        }
 
         /// <summary>
-        /// A copy of the row that was updated/inserted in the user's table. In the case of 
-        /// a delete, this is populated with a default(T) value. 
-        /// Note that there is a chance this data does not reflect the most recent version of the row in the user's table
-        /// There is also a chance that the data does not accurately reflect <see cref="ChangeType"/>. For example,
-        /// say that a row was updated and then deleted, and this SqlChangeTrackingEntry corresponds to the first change, the update.
-        /// In that case, ChangeType will be <see cref="WatcherChangeTypes.Changed"/>, but by the time the user table is queried
-        /// for the row, the row has been deleted, so Data will be default(T).
+        /// Specifies the type of change that occurred to the row. 
+        /// <see cref="SqlChangeType.Updated"/> corresponds to an update, 
+        /// <see cref="SqlChangeType.Inserted"/> corresponds to an insert, 
+        /// <see cref="SqlChangeType.Deleted"/> corresponds to a delete
         /// </summary>
-        public T Data { get; set; }
+        public SqlChangeType ChangeType { get; }
+
+        /// <summary>
+        /// A copy of the row that was updated/inserted in the user's table.
+        /// In the case that the row no longer exists in the user's table, Data is only populated with the primary key values
+        /// of the deleted row.
+        /// </summary>
+        public T Data { get; }
+    }
+
+    public enum SqlChangeType
+    {
+        Inserted,
+        Updated,
+        Deleted
     }
 }
--- src/SqlBinding/TriggerBinding/SqlTriggerParameterDescriptor.cs
+++ src/SqlBinding/TriggerBinding/SqlTriggerParameterDescriptor.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using Microsoft.Azure.WebJobs.Host.Protocols;
@@ -18,13 +18,11 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
         /// The reason the user's function was triggered. Specifies the table name that experienced changes
         /// as well as the time the changes were detected
         /// </summary>
-        /// <param name="arguments">
-        /// Stores the table name and time changes were detected
-        /// </param>
+        /// <param name="arguments">Unused</param>
         /// <returns>A string with the reason</returns>
         public override string GetTriggerReason(IDictionary<string, string> arguments)
         {
-            return string.Format("New changes on table {0} at {1}", this.TableName, DateTime.UtcNow.ToString("o"));
+            return $"New changes on table {TableName} at {DateTime.UtcNow.ToString("o")}";
         }
     }
 }
