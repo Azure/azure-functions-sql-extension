475c37f7c0aa491f0704978f217c4f336893342c
Sophia Tevosyan <t-sotevo@microsoft.com>
2020-07-31 16:49:52 -0700

    have the basic flow working

---
+++ src/SqlBinding/TriggerBinding/SqlTriggerParameterDescriptor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.WebJobs.Host.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Sql
+{
+    internal class SqlTriggerParameterDescriptor : TriggerParameterDescriptor
+    {
+        /// <summary>
+        /// Name of the table being monitored
+        /// </summary>
+        public string TableName { get; set; }
+
+        public override string GetTriggerReason(IDictionary<string, string> arguments)
+        {
+            return string.Format("New changes on table {0} at {1}", this.TableName, DateTime.UtcNow.ToString("o"));
+        }
+    }
+}
