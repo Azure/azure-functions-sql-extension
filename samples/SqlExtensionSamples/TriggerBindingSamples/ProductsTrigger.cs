dbeae95788d5c7b52bd431f5354b401ccadf2a82
Sophia Tevosyan <t-sotevo@microsoft.com>
2020-08-04 01:50:58 -0700

    can't get the converter working

--- samples/SqlExtensionSamples/TriggerBindingSamples/ProductsTrigger.cs
+++ samples/SqlExtensionSamples/TriggerBindingSamples/ProductsTrigger.cs
@@ -1,28 +1,60 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Sql;
+using System;
+using Microsoft.Data.SqlClient;
+using Microsoft.Azure.WebJobs.Description;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+using Microsoft.Azure.WebJobs.Host.Config;
+using Microsoft.Extensions.Configuration;
+using static Microsoft.Azure.WebJobs.Extensions.Sql.SqlConverters;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-using static SqlExtensionSamples.ProductUtilities;
 
-namespace SqlExtensionSamples.TriggerBindingSamples
+namespace Microsoft.Azure.WebJobs.Extensions.Sql
 {
-    public static class ProductsTrigger
+    /// <summary>
+    /// Exposes SQL input, output, and trigger bindings
+    /// </summary>
+    [Extension("sql")]
+    internal class SqlBindingConfigProvider : IExtensionConfigProvider
     {
-        [FunctionName("ProductsTrigger")]
-        public static void Run(
-            [SqlTrigger("[dbo].[Products]", ConnectionStringSetting = "SqlConnectionString")]
-            IEnumerable<SqlChangeTrackingEntry<Product>> changes,
-            ILogger logger)
+        private readonly IConfiguration _configuration;
+        private readonly ILoggerFactory _loggerFactory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SqlBindingConfigProvider/>"/> class.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if either parameter is null
+        /// </exception>
+        public SqlBindingConfigProvider(IConfiguration configuration, ILoggerFactory loggerFactory)
         {
-            foreach (var change in changes)
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
+        }
+
+        /// <summary>
+        /// Initializes the SQL binding rules
+        /// </summary>
+        /// <param name="context"> The config context </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if context is null
+        /// </exception>
+        public void Initialize(ExtensionConfigContext context)
+        {
+            if (context == null)
             {
-                Product product = change.Data;
-                logger.LogInformation($"Change occurred to Products table row: {change.ChangeType}");
-                logger.LogInformation($"ProductID: {product.ProductID}, Name: {product.Name}, Price: {product.Cost}");
+                throw new ArgumentNullException(nameof(context));
             }
+            var inputOutputRule = context.AddBindingRule<SqlAttribute>();
+            var converter = new SqlConverter(_configuration);
+            inputOutputRule.BindToInput<SqlCommand>(converter);
+            inputOutputRule.BindToInput<string>(typeof(SqlGenericsConverter<string>), _configuration);
+            inputOutputRule.BindToCollector<OpenType>(typeof(SqlAsyncCollectorBuilder<>), _configuration);
+            inputOutputRule.BindToInput<OpenType>(typeof(SqlGenericsConverter<>), _configuration);
+
+            var triggerRule = context.AddBindingRule<SqlTriggerAttribute>();
+            triggerRule.BindToTrigger(new SqlTriggerAttributeBindingProvider(_configuration, _loggerFactory));
         }
     }
 }
