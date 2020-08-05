e48ff8b054a6390f712991b78f2b6ad9f80c6869
Sophia Tevosyan <t-sotevo@microsoft.com>
2020-08-04 17:38:49 -0700

    everything seems to be working now

---
+++ src/SqlBinding/TriggerBinding/ChangeTableData.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Sql
+{
+    internal class ChangeTableData
+    {
+        // Change this to actually have fields for each of the columns
+        public List<Dictionary<string, string>> workerTableRows { get; set; }
+
+        public Dictionary<Dictionary<string, string>, string> whereChecks { get; set; }
+    }
+}
