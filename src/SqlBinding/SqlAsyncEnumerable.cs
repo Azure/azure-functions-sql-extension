6dd33f62de055d09e080d55a28427bc735b950fb
Sophia Tevosyan <t-sotevo@microsoft.com>
2020-08-11 15:47:10 -0700

    added getting the user table data to the query where we check for changes

--- src/SqlBinding/SqlAsyncEnumerable.cs
+++ src/SqlBinding/SqlAsyncEnumerable.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Sql
 {
+    /// <typeparam name="T">A user-defined POCO that represents a row of the user's table</typeparam>
     internal class SqlAsyncEnumerable<T> : IAsyncEnumerable<T>
     {
         private readonly SqlConnection _connection;
