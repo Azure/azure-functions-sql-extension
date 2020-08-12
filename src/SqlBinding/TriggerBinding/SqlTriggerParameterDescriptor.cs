78538a17475df8e91af562eae6cdf0bc80287e14
Sophia Tevosyan <t-sotevo@microsoft.com>
2020-08-11 18:42:11 -0700

    minor cleanup

--- src/SqlBinding/TriggerBinding/SqlTriggerParameterDescriptor.cs
+++ src/SqlBinding/TriggerBinding/SqlTriggerParameterDescriptor.cs
@@ -18,13 +18,11 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
         /// The reason the user's function was triggered. Specifies the table name that experienced changes
         /// as well as the time the changes were detected
         /// </summary>
-        /// <param name="arguments">
-        /// Stores the table name and time changes were detected
-        /// </param>
+        /// <param name="arguments">Unused</param>
         /// <returns>A string with the reason</returns>
         public override string GetTriggerReason(IDictionary<string, string> arguments)
         {
-            return string.Format("New changes on table {0} at {1}", this.TableName, DateTime.UtcNow.ToString("o"));
+            return $"New changes on table {TableName} at {DateTime.UtcNow.ToString("o")}";
         }
     }
 }
