407eb57ac9510443bc3415bd96da06fedfcd206b
Chris Gillum <cgillum@microsoft.com>
2020-10-15 17:11:59 -0700

    Fixed test build warnings and errors (#29)

--- test/SQLExtension.tests/SQLOutputBindingTests.cs
+++ test/SQLExtension.tests/SQLOutputBindingTests.cs
@@ -2,9 +2,11 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Sql;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using Xunit;
 
@@ -12,23 +14,23 @@ namespace SqlExtension.Tests
 {
     public class SqlOutputBindingTests
     {
-        private static Mock<IConfiguration> config = new Mock<IConfiguration>();
+        private static readonly Mock<IConfiguration> config = new Mock<IConfiguration>();
 
         [Fact]
         public void TestNullCollectorConstructorArguments()
         {
             var arg = new SqlAttribute(string.Empty);
-            Assert.Throws<ArgumentNullException>(() => new SqlAsyncCollector<string>(config.Object, null));
-            Assert.Throws<ArgumentNullException>(() => new SqlAsyncCollector<string>(null, arg));
+            Assert.Throws<ArgumentNullException>(() => new SqlAsyncCollector<string>(config.Object, null, NullLoggerFactory.Instance));
+            Assert.Throws<ArgumentNullException>(() => new SqlAsyncCollector<string>(null, arg, NullLoggerFactory.Instance));
         }
 
         [Fact]
-        public void TestAddAsync()
+        public async Task TestAddAsync()
         {
             // Really a pretty silly test. Just confirms that the SQL connection is only opened when FlushAsync is called,
             // because otherwise we would get an exception in AddAsync (since the SQL connection in the wrapper is null)
             var arg = new SqlAttribute(string.Empty);
-            var collector = new SqlAsyncCollector<TestData>(config.Object, arg);
+            var collector = new SqlAsyncCollector<TestData>(config.Object, arg, NullLoggerFactory.Instance);
             var data = new TestData
             {
                 ID = 1,
@@ -36,7 +38,7 @@ namespace SqlExtension.Tests
                 Cost = 10,
                 Timestamp = new DateTime(2019, 11, 22, 6, 32, 15)
             };
-            collector.AddAsync(data);
+            await collector.AddAsync(data);
         }
     }
 }
