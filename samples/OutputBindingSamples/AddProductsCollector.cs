53fae3fb7d13ec35a38b83a38444162ad0674000
Charles Gagnon <chgagnon@microsoft.com>
2021-10-07 13:28:10 -0700

    Refactor folder structure (#116)
    * Refactor folder structure
    
    * Fix tests
    
    * remove folder + comment
    
    * Fix output message
    
    * Add debug logging
    
    * Add back local.settings.json

---
+++ samples/OutputBindingSamples/AddProductsCollector.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.Sql.Samples.Common;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Sql.Samples.OutputBindingSamples
+{
+    public static class AddProductsCollector
+    {
+        [FunctionName("AddProductsCollector")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "addproducts-collector")]
+            HttpRequest req,
+            [Sql("dbo.Products", ConnectionStringSetting = "SqlConnectionString")] ICollector<Product> products)
+        {
+            List<Product> newProducts = ProductUtilities.GetNewProducts(5000);
+            foreach (Product product in newProducts)
+            {
+                products.Add(product);
+            }
+            return new CreatedResult($"/api/addproducts-collector", "done");
+        }
+    }
+}
