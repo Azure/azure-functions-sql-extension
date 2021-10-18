f80f0cd7f57567f3be5b6c6992d02ac5bbe9b679
Lucy Zhang <luczhan@microsoft.com>
2021-10-18 15:44:52 -0700

    Check for extra columns before upsert (#128)
    * check for extra columns before upsert
    
    * add test for upsert with extra columns
    
    * fix test
    
    * extra line
    
    * Move extra columns binding to Test
    
    * add comment

--- src/SqlAsyncCollector.cs
+++ src/SqlAsyncCollector.cs
@@ -142,6 +142,13 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
                 cachedTables.Set(cacheKey, tableInfo, policy);
             }
 
+            IEnumerable<string> extraProperties = GetExtraProperties(tableInfo.Columns);
+            if (extraProperties.Any())
+            {
+                string message = $"The following properties in {typeof(T)} do not exist in the table {fullDatabaseAndTableName}: {string.Join(", ", extraProperties.ToArray())}.";
+                throw new InvalidOperationException(message);
+            }
+
             int batchSize = 1000;
             await connection.OpenAsync();
             foreach (IEnumerable<T> batch in rows.Batch(batchSize))
@@ -155,6 +162,20 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
             }
             await connection.CloseAsync();
         }
+
+        /// <summary>
+        /// Checks if any properties in T do not exist as columns in the table
+        /// to upsert to and returns the extra property names in a List.
+        /// </summary>
+        /// <param name="columns"> The columns of the table to upsert to </param>
+        /// <returns>List of property names that don't exist in the table</returns>
+        private static IEnumerable<string> GetExtraProperties(IDictionary<string, string> columns)
+        {
+            return typeof(T).GetProperties().ToList()
+                .Where(prop => !columns.ContainsKey(prop.Name))
+                .Select(prop => prop.Name);
+        }
+
         /// <summary>
         /// Generates T-SQL for data to be upserted using Merge.
         /// This needs to be regenerated for every batch to upsert.
