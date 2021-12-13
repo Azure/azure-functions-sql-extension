28bbedf2385fc169a2ee483867e4dd027bf4c8c4
Charles Gagnon <chgagnon@microsoft.com>
2021-12-13 11:07:13 -0800

    Add SqlObject helper class (#183)

--- src/SqlBindingUtilities.cs
+++ src/SqlBindingUtilities.cs
@@ -4,10 +4,8 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.IO;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
-using Microsoft.SqlServer.TransactSql.ScriptDom;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Sql
 {
@@ -181,50 +179,6 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
         public static string AsQuotedString(this string s)
         {
             return $"'{s.Replace("'", "''")}'";
-        }
-
-        /// <summary>
-        /// Use ScriptDom to parse schema and tableName and return them with quotes.
-        /// If there is no schema in fullName, SCHEMA_NAME() is returned as schema.
-        /// </summary>
-        /// <param name="fullName">
-        /// Full name of table, including schema (if exists).
-        /// </param>
-        public static void GetTableAndSchema(string fullName, out string quotedSchema, out string quotedTableName)
-        {
-            var parser = new TSql150Parser(false);
-            var stringReader = new StringReader(fullName);
-            SchemaObjectName tree = parser.ParseSchemaObjectName(stringReader, out IList<ParseError> errors);
-
-            if (errors.Count > 0)
-            {
-                string errorMessages = "Encountered error(s) while parsing schema and table name:\n";
-                foreach (ParseError err in errors)
-                {
-                    errorMessages += $"{err.Message}\n";
-                }
-                throw new InvalidOperationException(errorMessages);
-            }
-
-            var visitor = new QuotedTSqlFragmentVisitor();
-            tree.Accept(visitor);
-            quotedSchema = visitor.quotedSchema;
-            quotedTableName = visitor.quotedTableName;
-        }
-
-        /// <summary>
-        /// Get the schema and table name with quotes from the SchemaObjectName.  
-        /// </summary>
-        private class QuotedTSqlFragmentVisitor : TSqlFragmentVisitor
-        {
-            internal string quotedSchema;
-            internal string quotedTableName;
-
-            public override void Visit(SchemaObjectName node)
-            {
-                this.quotedSchema = node.SchemaIdentifier != null ? node.SchemaIdentifier.Value.AsQuotedString() : "SCHEMA_NAME()";
-                this.quotedTableName = node.BaseIdentifier.Value.AsQuotedString();
-            }
         }
     }
 }
