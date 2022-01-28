3dc8490f6a625c6eecfb2d658d47896b66ef1178
chgagnon <chgagnon@microsoft.com>
2022-01-28 11:11:55 -0800

    Add machine ID

--- src/Telemetry/TelemetryCommonProperties.cs
+++ src/Telemetry/TelemetryCommonProperties.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
@@ -14,18 +15,33 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Telemetry
             string productVersion)
         {
             this._productVersion = productVersion;
+            this._userLevelCacheWriter = new UserLevelCacheWriter();
         }
+
+        private readonly UserLevelCacheWriter _userLevelCacheWriter;
 
         private const string OSVersion = "OSVersion";
         private const string ProductVersion = "ProductVersion";
+        private const string MachineId = "MachineId";
 
         public Dictionary<string, string> GetTelemetryCommonProperties()
         {
             return new Dictionary<string, string>
             {
                 {OSVersion, RuntimeInformation.OSDescription},
-                {ProductVersion, this._productVersion}
+                {ProductVersion, this._productVersion},
+                {MachineId, this.GetMachineId()},
             };
+        }
+
+        private string GetMachineId()
+        {
+            return this._userLevelCacheWriter.RunWithCache(MachineId, () =>
+            {
+                // For now just use random GUID. Typically hashed MAC address is used but that's not
+                // something we need currently - a GUID per user is sufficient.
+                return Guid.NewGuid().ToString();
+            });
         }
     }
 }
---
+++ src/Telemetry/UserLevelCacheWriter.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Sql.Telemetry
+{
+    public sealed class UserLevelCacheWriter
+    {
+        private const string AzureFunctionsSqlBindingsProfileDirectoryName = ".azurefunctions-sqlbindings";
+        private readonly string _azureFunctionsSqlBindingsTryUserProfileFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), AzureFunctionsSqlBindingsProfileDirectoryName);
+
+        public string RunWithCache(string cacheKey, Func<string> getValueToCache)
+        {
+            string cacheFilepath = this.GetCacheFilePath(cacheKey);
+            try
+            {
+                if (!File.Exists(cacheFilepath))
+                {
+                    if (!Directory.Exists(this._azureFunctionsSqlBindingsTryUserProfileFolderPath))
+                    {
+                        Directory.CreateDirectory(this._azureFunctionsSqlBindingsTryUserProfileFolderPath);
+                    }
+
+                    string runResult = getValueToCache();
+
+                    File.WriteAllText(cacheFilepath, runResult);
+                    return runResult;
+                }
+                else
+                {
+                    return File.ReadAllText(cacheFilepath);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is UnauthorizedAccessException
+                    || ex is PathTooLongException
+                    || ex is IOException)
+                {
+                    return getValueToCache();
+                }
+
+                throw;
+            }
+        }
+
+        private string GetCacheFilePath(string cacheKey)
+        {
+            return Path.Combine(this._azureFunctionsSqlBindingsTryUserProfileFolderPath, $"{cacheKey}.azureFunctionsSqlBindingsTryUserLevelCache");
+        }
+    }
+}
