7f45f4a046aad6ab8c744684f47d06696d9611a1
chgagnon <chgagnon@microsoft.com>
2022-02-03 16:51:07 -0800

    Filter out cloud RoleInstance

---
+++ src/Telemetry/TelemetryInitializer.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Sql.Telemetry
+{
+    public class TelemetryInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            // Filter out the Cloud RoleInstance - we don't care about that field and
+            // it can contain information such as the machine and domain name of the client
+            telemetry.Context.Cloud.RoleInstance = "-";
+        }
+    }
+
+}
