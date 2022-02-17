f2ecb262b21892e935d35e120a861ffe4d714802
Lucy Zhang <luczhan@microsoft.com>
2022-02-17 07:08:08 -0800

    Add logs for # of rows queried and upserted (#211)
    * add logs for # of rows queried and upserted
    
    * fix unit test
    
    * create  logger in provider

--- src/SqlAsyncCollectorBuilder.cs
+++ src/SqlAsyncCollectorBuilder.cs
@@ -9,17 +9,17 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
     internal class SqlAsyncCollectorBuilder<T> : IConverter<SqlAttribute, IAsyncCollector<T>>
     {
         private readonly IConfiguration _configuration;
-        private readonly ILoggerFactory _loggerFactory;
+        private readonly ILogger _logger;
 
-        public SqlAsyncCollectorBuilder(IConfiguration configuration, ILoggerFactory loggerFactory)
+        public SqlAsyncCollectorBuilder(IConfiguration configuration, ILogger logger)
         {
             this._configuration = configuration;
-            this._loggerFactory = loggerFactory;
+            this._logger = logger;
         }
 
         IAsyncCollector<T> IConverter<SqlAttribute, IAsyncCollector<T>>.Convert(SqlAttribute attribute)
         {
-            return new SqlAsyncCollector<T>(this._configuration, attribute, this._loggerFactory);
+            return new SqlAsyncCollector<T>(this._configuration, attribute, this._logger);
         }
     }
 }
--- test/Unit/SqlInputBindingTests.cs
+++ test/Unit/SqlInputBindingTests.cs
@@ -19,6 +19,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Unit
     {
         private static readonly Mock<IConfiguration> config = new Mock<IConfiguration>();
         private static readonly Mock<ILoggerFactory> loggerFactory = new Mock<ILoggerFactory>();
+        private static readonly Mock<ILogger> logger = new Mock<ILogger>();
         private static readonly SqlConnection connection = new SqlConnection();
 
         [Fact]
@@ -27,7 +28,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Unit
             Assert.Throws<ArgumentNullException>(() => new SqlBindingConfigProvider(null, loggerFactory.Object));
             Assert.Throws<ArgumentNullException>(() => new SqlBindingConfigProvider(config.Object, null));
             Assert.Throws<ArgumentNullException>(() => new SqlConverter(null));
-            Assert.Throws<ArgumentNullException>(() => new SqlGenericsConverter<string>(null));
+            Assert.Throws<ArgumentNullException>(() => new SqlGenericsConverter<string>(null, logger.Object));
         }
 
         [Fact]
@@ -219,7 +220,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Unit
         public async void TestWellformedDeserialization()
         {
             var arg = new SqlAttribute(string.Empty);
-            var converter = new Mock<SqlGenericsConverter<TestData>>(config.Object);
+            var converter = new Mock<SqlGenericsConverter<TestData>>(config.Object, logger.Object);
             string json = "[{ \"ID\":1,\"Name\":\"Broom\",\"Cost\":32.5,\"Timestamp\":\"2019-11-22T06:32:15\"},{ \"ID\":2,\"Name\":\"Brush\",\"Cost\":12.3," +
                 "\"Timestamp\":\"2017-01-27T03:13:11\"},{ \"ID\":3,\"Name\":\"Comb\",\"Cost\":100.12,\"Timestamp\":\"1997-05-03T10:11:56\"}]";
             converter.Setup(_ => _.BuildItemFromAttributeAsync(arg)).ReturnsAsync(json);
@@ -256,7 +257,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Unit
         public async void TestMalformedDeserialization()
         {
             var arg = new SqlAttribute(string.Empty);
-            var converter = new Mock<SqlGenericsConverter<TestData>>(config.Object);
+            var converter = new Mock<SqlGenericsConverter<TestData>>(config.Object, logger.Object);
 
             // SQL data is missing a field
             string json = "[{ \"ID\":1,\"Name\":\"Broom\",\"Timestamp\":\"2019-11-22T06:32:15\"}]";
