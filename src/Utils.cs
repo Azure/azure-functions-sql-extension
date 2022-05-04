0d4b897aa15a061db2fe32374d265a7465913e83
chgagnon <chgagnon@microsoft.com>
2022-05-04 16:35:43 -0700

    remove unused

--- src/Utils.cs
+++ src/Utils.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
-using System.Linq;
 using Microsoft.Extensions.Configuration;
 using MoreLinq;
 using Newtonsoft.Json.Linq;
