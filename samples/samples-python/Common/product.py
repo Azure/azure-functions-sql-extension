03f3360ae885791e31f0cf6660dfb9dc6640c698
chgagnon <chgagnon@microsoft.com>
2022-05-06 11:58:56 -0700

    remove unneeded

--- samples/samples-python/Common/product.py
+++ samples/samples-python/Common/product.py
@@ -4,12 +4,6 @@
 import collections
 
 class Product(collections.UserDict):
-    def __getitem__(self, key):
-        return collections.UserDict.__getitem__(self, key)
-
-    def __setitem__(self, key, value):
-        return collections.UserDict.__setitem__(self, key, value)
-
     def __init__(self, productId, name, cost):
         super().__init__()
         self['ProductId'] = productId
