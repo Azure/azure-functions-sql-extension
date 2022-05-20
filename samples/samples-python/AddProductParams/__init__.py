020c767179ea9e0460d4c874c31275963f359939
luczhan <luczhan@microsoft.com>
2022-05-20 11:01:25 -0700

    remove extra change

--- samples/samples-python/AddProductParams/__init__.py
+++ samples/samples-python/AddProductParams/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import azure.functions as funcs
+import azure.functions as func
 from Common.product import Product
 
 def main(req: func.HttpRequest, product: func.Out[func.SqlRow]) -> func.HttpResponse:
