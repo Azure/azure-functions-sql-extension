0b9a42800d18c9bbd5aa562c7542a52845ddce68
Charles Gagnon <chgagnon@microsoft.com>
2022-05-20 13:54:38 -0700

    Add copyright to JS samples (#255)
    * Add lint
    
    * Remove duplicate
    
    * Fix path
    
    * Add headers
    
    * format
    
    * remove unneeded

--- samples/samples-js/AddProduct/index.js
+++ samples/samples-js/AddProduct/index.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 // Upsert the product, which will insert it into the Products table if the primary key (ProductId) for that item doesn't exist.
 // If it does then update it to have the new name and cost.
 module.exports = async function (context, req) {
--- samples/samples-js/AddProductWithIdentityColumn/index.js
+++ samples/samples-js/AddProductWithIdentityColumn/index.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
