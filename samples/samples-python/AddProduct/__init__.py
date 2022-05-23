443b0e9ccc70138e0975e1d793666638fc5fbde1
Lucy Zhang <luczhan@microsoft.com>
2022-05-23 15:57:11 -0700

    Add Python samples (#253)
    * Initial samples
    
    * cleanup
    
    * AddProductsArray
    
    * Add product class
    
    * remove unneeded
    
    * More samples + cleanup
    
    * update python samples to match js samples
    
    * update param name and runtime version
    
    * remove extra change
    
    Co-authored-by: chgagnon <chgagnon@microsoft.com>

---
+++ samples/samples-python/AddProduct/__init__.py
@@ -0,0 +1,22 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import azure.functions as func
+import json
+
+def main(req: func.HttpRequest, product: func.Out[func.SqlRow]) -> func.HttpResponse:
+    """Upsert the product, which will insert it into the Products table if the primary key (ProductId) for that item doesn't exist.
+    If it does then update it to have the new name and cost.
+    """
+
+    # Note that this expects the body to be a JSON object which
+    # have a property matching each of the columns in the table to upsert to.
+    body = json.loads(req.get_body())
+    row = func.SqlRow.from_dict(body)
+    product.set(row)
+
+    return func.HttpResponse(
+        body=req.get_body(),
+        status_code=201,
+        mimetype="application/json"
+    )
---
+++ samples/samples-python/AddProductsArray/__init__.py
@@ -0,0 +1,22 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import azure.functions as func
+import json
+
+def main(req: func.HttpRequest, products: func.Out[func.SqlRowList]) -> func.HttpResponse:
+    """This function upserts the products, which will insert them into the Products table if the primary key (ProductId) for that item doesn't exist.
+    If it does then update it to have the new name and cost.
+    """
+
+    # Note that this expects the body to be an array of JSON objects which
+    # have a property matching each of the columns in the table to upsert to.
+    body = json.loads(req.get_body())
+    rows = func.SqlRowList(map(lambda r: func.SqlRow.from_dict(r), body))
+    products.set(rows)
+
+    return func.HttpResponse(
+        body=req.get_body(),
+        status_code=201,
+        mimetype="application/json"
+    )
