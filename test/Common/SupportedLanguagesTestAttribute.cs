d4ce15e999e32c899da70a08366d96241697db3c
Madhuri Koripalli <makoripa@microsoft.com>
2022-06-06 12:20:03 -0700

    add comments

--- test/Common/SupportedLanguagesTestAttribute.cs
+++ test/Common/SupportedLanguagesTestAttribute.cs
@@ -6,17 +6,21 @@ using Xunit.Sdk;
 using System.Reflection;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using System.Linq;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Common
 {
+    /// <summary>
+    /// Base attribute class for adding language parameter data to tests. This allows
+    /// tests to be run against all the supported languages list that are specified in the SupportedLanguages
+    /// list of this class (ex: CSharp, JavaScript)
+    /// </summary>
     public abstract class LanguageTestAttribute : DataAttribute
     {
+        // add all the list of supported languages.
         protected enum SupportedLanguages
         {
             CSharp
-            /*JavaScript,
-            Python,
-             TypeScript */
         }
         protected readonly Dictionary<string, List<List<object>>> testData = new Dictionary<string, List<List<object>>>();
         protected readonly string _propertyName = "data";
@@ -37,7 +41,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Common
     public class SupportedLanguagesAttribute : LanguageTestAttribute
     {
         /// <summary>
-        /// Load supported languages for inline data for a theory
+        /// Load supported languages as test parameters data for the test.
         /// </summary>
         public SupportedLanguagesAttribute()
         {
@@ -50,33 +54,21 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Common
             this.testData.Add(this._propertyName, langData);
         }
         /// <summary>
-        /// Load data from a inline data for a theory
+        /// Load the test parameters data for each language for inline use of the test.
         /// </summary>
-        /// <param name="n">The number of products to insert for the test</param>
-        /// <param name="cost">The cost of the products for the test</param>
-        public SupportedLanguagesAttribute(int n, int cost)
+        /// <param name="args">The test parameters to insert inline for the test</param>
+        public SupportedLanguagesAttribute(params object[] args)
         {
             var langData = new List<List<object>>();
-            foreach (string lang in Enum.GetNames(typeof(SupportedLanguages)))
-            {
-                var listOfValues = new List<object>(3) { n, cost, lang };
-                langData.Add(listOfValues);
-            }
-            this.testData.Add(this._propertyName, langData);
-        }
+            var listOfValues = new List<object>();
 
-        /// <summary>
-        /// Load data from a inline data for a theory
-        /// </summary>
-        /// <param name="id">The id of products for the test</param>
-        /// <param name="name">The name of products for the test</param>
-        /// <param name="cost">The cost of the product for the test</param>
-        public SupportedLanguagesAttribute(int id, string name, int cost)
-        {
-            var langData = new List<List<object>>();
             foreach (string lang in Enum.GetNames(typeof(SupportedLanguages)))
             {
-                var listOfValues = new List<object>(4) { id, name, cost, lang };
+                foreach (object val in args)
+                {
+                    listOfValues.Add(val);
+                }
+                listOfValues.Add(lang);
                 langData.Add(listOfValues);
             }
             this.testData.Add(this._propertyName, langData);
@@ -86,16 +78,28 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Common
     public class UnsupportedLanguagesAttribute : LanguageTestAttribute
     {
         /// <summary>
-        /// Load supported languages for inline data for a theory
+        /// Load only supported languages excluding the ones from the provided parameters.
         /// </summary>
-        public UnsupportedLanguagesAttribute(string[] args)
+        public UnsupportedLanguagesAttribute(params object[] argsWithUnsupportedLangs)
         {
             var langData = new List<List<object>>();
+            var listOfValues = new List<object>();
+
+            // get the unsupported list of languages from the argsWithUnsupportedLangs
+            string[] argsAsString = Array.ConvertAll(argsWithUnsupportedLangs, x => x.ToString());
+            IEnumerable<string> unsupportedList = argsAsString.Intersect(Enum.GetNames(typeof(SupportedLanguages)));
+            // get the actual parameters filtering out the unsupported language list.
+            object[] args = Array.FindAll(argsWithUnsupportedLangs, x => !unsupportedList.Contains(x));
             foreach (string lang in Enum.GetNames(typeof(SupportedLanguages)))
             {
-                if (Array.IndexOf(args, lang) == -1)
+                if (!unsupportedList.Contains(lang))
                 {
-                    langData.Add(new List<object>(1) { lang });
+                    foreach (object val in args)
+                    {
+                        listOfValues.Add(val);
+                    }
+                    listOfValues.Add(lang);
+                    langData.Add(listOfValues);
                 }
             }
             this.testData.Add(this._propertyName, langData);
