76575f29e964f3487684b84e279444279296f5c2
Charles Gagnon <chgagnon@microsoft.com>
2022-08-31 14:18:18 -0700

    Fix localization/serialization issues (#329)
    * Fix localization issues
    
    * split tests & cleanup
    
    * Replace file copy with itemgroup

---
+++ test/Common/ProductColumnTypes.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Common
+{
+    public class ProductColumnTypes
+    {
+        public int ProductID { get; set; }
+
+        public DateTime Datetime { get; set; }
+
+        public DateTime Datetime2 { get; set; }
+    }
+}
