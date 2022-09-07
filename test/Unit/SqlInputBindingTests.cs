85e5e7faff59e9c3be86468be544e489585dfbc0
Maddy Koripalli <makoripa@microsoft.com>
2022-09-06 21:53:32 -0700

    add null check

--- test/Unit/SqlInputBindingTests.cs
+++ test/Unit/SqlInputBindingTests.cs
@@ -27,9 +27,9 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Unit
         public void TestNullConfiguration()
         {
             Assert.Throws<ArgumentNullException>(() => new SqlBindingConfigProvider(null, loggerFactory.Object));
+            Assert.Throws<ArgumentNullException>(() => new SqlBindingConfigProvider(config.Object, null));
             Assert.Throws<ArgumentNullException>(() => new SqlConverter(null, logger.Object));
             Assert.Throws<ArgumentNullException>(() => new SqlGenericsConverter<string>(null, logger.Object));
-
         }
 
         [Fact]
