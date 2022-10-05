9c790dea264c2123287d448d60c0ea0132fb0f7b
Maddy Koripalli <makoripa@microsoft.com>
2022-10-05 11:59:33 -0700

    add new samples-outofproc

---
+++ samples/samples-outofproc/Program.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker.Sql;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SamplesOutOfProc
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // #if DEBUG
+            //     Debugger.Launch();
+            // #endif
+            //<docsnippet_startup>
+            var host = new HostBuilder()
+                //<docsnippet_configure_defaults>
+                .ConfigureFunctionsWorkerDefaults(builder =>
+                {
+                    builder
+                        .AddApplicationInsights()
+                        .AddApplicationInsightsLogger();
+                })
+                //</docsnippet_configure_defaults>
+                //<docsnippet_dependency_injection>
+                .ConfigureServices(s =>
+                {
+                    s.AddSingleton<IHttpResponderService, DefaultHttpResponderService>();
+                })
+                //</docsnippet_dependency_injection>
+                .Build();
+            //</docsnippet_startup>
+
+            //<docsnippet_host_run>
+            await host.RunAsync();
+            //</docsnippet_host_run>
+        }
+    }
+}
