86d5600a504c4fdb00aa7ce719d479bc4d246694
Maddy <12754347+MaddyDev@users.noreply.github.com>
2022-10-19 14:54:07 -0700

    Add sql extension exposing binding metadata for out of proc (#382)
    * add out of proc intial steps
    
    * rename namespace
    
    * update csproj
    
    * add outofproc proj to sln
    
    * remove ci.yml and extensions-ci
    
    * update xml
    
    * undo package lock updates
    
    * clean up urls
    
    * update comments
    
    * fix indentation
    
    * don't hardcode version
    
    * add nuget version range syntax
    
    * address comments
    
    * address comments part2
    
    * missed set CommandText on output attribute
    
    * remove unused param from xml
    
    * fix formatting

---
+++ Worker.Extension.Sql/src/SqlOutputAttribute.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+
+namespace Microsoft.Azure.Functions.Worker.Extension.Sql
+{
+    public class SqlOutputAttribute : OutputBindingAttribute
+    {
+        /// <summary>
+        /// Creates an instance of the <see cref="SqlAttribute"/>, specifying the Sql attributes
+        /// the function supports.
+        /// </summary>
+        /// <param name="commandText">The text of the command.</param>
+        public SqlOutputAttribute(string commandText)
+        {
+            this.CommandText = commandText;
+        }
+
+        /// <summary>
+        /// The name of the app setting where the SQL connection string is stored
+        /// (see https://docs.microsoft.com/dotnet/api/microsoft.data.sqlclient.sqlconnection).
+        /// The attributes specified in the connection string are listed here
+        /// https://docs.microsoft.com/dotnet/api/microsoft.data.sqlclient.sqlconnection.connectionstring
+        /// For example, to create a connection to the "TestDB" located at the URL "test.database.windows.net" using a User ID and password,
+        /// create a ConnectionStringSetting with a name like SqlServerAuthentication. The value of the SqlServerAuthentication app setting
+        /// would look like "Data Source=test.database.windows.net;Database=TestDB;User ID={userid};Password={password}".
+        /// </summary>
+        public string ConnectionStringSetting { get; set; }
+
+        /// <summary>
+        /// The table name to upsert the values to.
+        /// </summary>
+        public string CommandText { get; set; }
+
+        /// <summary>
+        /// Specifies <see cref="CommandText"/> as Text.
+        /// </summary>
+        public System.Data.CommandType CommandType { get; } = System.Data.CommandType.Text;
+    }
+}
