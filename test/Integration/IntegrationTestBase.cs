5bb1701992bd2c3f8818ee47899daca0c08ff297
Vasu Bhog <vabhog@microsoft.com>
2022-10-21 15:24:02 -0700

    include runtime

--- test/Integration/IntegrationTestBase.cs
+++ test/Integration/IntegrationTestBase.cs
@@ -9,6 +9,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
