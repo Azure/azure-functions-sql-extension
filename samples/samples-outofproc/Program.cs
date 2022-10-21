d0ca0781a920ce6a25b68d94d4b1970066fb78b7
Maddy Koripalli <makoripa@microsoft.com>
2022-10-21 12:03:53 -0700

    add dependencies to Directory.package

--- samples/samples-outofproc/Program.cs
+++ samples/samples-outofproc/Program.cs
@@ -1,41 +1,25 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 
 namespace Microsoft.Azure.WebJobs.Extensions.Sql.SamplesOutOfProc
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            // #if DEBUG
-            //     Debugger.Launch();
-            // #endif
-            //<docsnippet_startup>
-            var host = new HostBuilder()
-                //<docsnippet_configure_defaults>
-                .ConfigureFunctionsWorkerDefaults(builder =>
-                {
-                    builder
-                        .AddApplicationInsights()
-                        .AddApplicationInsightsLogger();
-                })
-                //</docsnippet_configure_defaults>
-                //<docsnippet_dependency_injection>
+            IHost host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
                 .ConfigureServices(s =>
                 {
                     s.AddSingleton<IHttpResponderService, DefaultHttpResponderService>();
                 })
-                //</docsnippet_dependency_injection>
                 .Build();
-            //</docsnippet_startup>
 
-            //<docsnippet_host_run>
-            await host.RunAsync();
-            //</docsnippet_host_run>
+            host.Run();
         }
     }
 }
