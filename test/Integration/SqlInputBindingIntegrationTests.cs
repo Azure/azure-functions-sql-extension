89f7cbb79b538653a48ad3b1dac2d73d4143e536
Vasu Bhog <vabhog@microsoft.com>
2022-10-27 14:58:33 -0700

    fix build

--- test/Integration/SqlInputBindingIntegrationTests.cs
+++ test/Integration/SqlInputBindingIntegrationTests.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
 using System.Net.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
