96b73c69a80074dcbc1110b9eac52bdfa80a7848
Vasu Bhog <vabhog@microsoft.com>
2022-10-31 15:07:04 -0700

    add debug add product test

--- samples/samples-powershell/AddProductParams/run.ps1
+++ samples/samples-powershell/AddProductParams/run.ps1
@@ -6,18 +6,6 @@ param($Request)
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell function with SQL Output Binding processed a request."
 
-# Update req_body with the body of the request
-# Note that this expects the body to be a JSON object or array of objects 
-# which have a property matching each of the columns in the table to upsert to.
-$req_body = @{ 
-    "productId"= $Request.Body.productId;
-    "name"= $Request.Body.name;
-    "cost"= $Request.Body.cost;
-};
- 
-Write-Host "REQUEST BODY"
-Write-Host $req_body
-
 $req_query = @{ 
     "productId"= $Request.QUERY.productId;
     "name"= $Request.QUERY.name;
@@ -26,10 +14,9 @@ $req_query = @{
 Write-Host "REQUEST QUERY"
 Write-Host $req_query
 
-
 # Assign the value we want to pass to the SQL Output binding. 
 # The -Name value corresponds to the name property in the function.json for the binding
-Push-OutputBinding -Name product -Value $req_body
+Push-OutputBinding -Name product -Value $req_query  ´
 
 # Assign the value to return as the HTTP response. 
 # The -Name value matches the name property in the function.json for the binding
