238ce8337fe9d76a50ab07eacf7667f956673087
Charles Gagnon <chgagnon@microsoft.com>
2022-11-02 16:34:19 -0700

    Add operation

--- test/Integration/SqlTriggerBindingIntegrationTests.cs
+++ test/Integration/SqlTriggerBindingIntegrationTests.cs
@@ -565,7 +565,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Integration
             await actions();
 
             // Now wait until either we timeout or we've gotten all the expected changes, whichever comes first
-            Console.WriteLine($"[{DateTime.UtcNow:u}] Waiting for product changes ({timeoutMs}ms)");
+            Console.WriteLine($"[{DateTime.UtcNow:u}] Waiting for {operation} changes ({timeoutMs}ms)");
             await taskCompletion.Task.TimeoutAfter(TimeSpan.FromMilliseconds(timeoutMs), $"Timed out waiting for {operation} changes.");
 
             // Unhook handler since we're done monitoring these changes so we aren't checking other changes done later
