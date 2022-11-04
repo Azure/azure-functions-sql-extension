ab38c83728e26c44e4211798e9f06cbc9a548c75
Charles Gagnon <chgagnon@microsoft.com>
2022-11-04 12:48:59 -0700

    updates

--- performance/SqlBindingBenchmarks.cs
+++ performance/SqlBindingBenchmarks.cs
@@ -9,12 +9,12 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Performance
     {
         public static void Main()
         {
-            // BenchmarkRunner.Run<SqlInputBindingPerformance>();
-            // BenchmarkRunner.Run<SqlOutputBindingPerformance>();
-            // BenchmarkRunner.Run<SqlTriggerBindingPerformance>();
-            // BenchmarkRunner.Run<SqlTriggerBindingPerformance_BatchOverride>();
-            // BenchmarkRunner.Run<SqlTriggerBindingPerformance_PollingIntervalOverride>();
-            // BenchmarkRunner.Run<SqlTriggerPerformance_Overrides>();
+            BenchmarkRunner.Run<SqlInputBindingPerformance>();
+            BenchmarkRunner.Run<SqlOutputBindingPerformance>();
+            BenchmarkRunner.Run<SqlTriggerBindingPerformance>();
+            BenchmarkRunner.Run<SqlTriggerBindingPerformance_BatchOverride>();
+            BenchmarkRunner.Run<SqlTriggerBindingPerformance_PollingIntervalOverride>();
+            BenchmarkRunner.Run<SqlTriggerPerformance_Overrides>();
             BenchmarkRunner.Run<SqlTriggerBindingPerformance_Parallelization>();
         }
     }
