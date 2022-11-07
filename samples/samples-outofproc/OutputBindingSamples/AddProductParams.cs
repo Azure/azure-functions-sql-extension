3efb8c18c312503db2d9d45ad9a32cfd22924b72
Maddy <12754347+MaddyDev@users.noreply.github.com>
2022-11-07 12:01:52 -0800

    samples for out of proc sql bindings (#441)
    * add out of proc intial steps
    
    * rename namespace
    
    * update csproj
    
    * add outofproc proj to sln
    
    * add new samples-outofproc
    
    * remove ci.yml and extensions-ci
    
    * update xml
    
    * samples-outofproc
    
    * undo package lock updates
    
    * clean up urls
    
    * update comments
    
    * fix indentation
    
    * don't hardcode version
    
    * add nuget version range syntax
    
    * remove samplesOutofProc project
    
    * address comments
    
    * address comments part2
    
    * missed set CommandText on output attribute
    
    * remove unused param from xml
    
    * fix formatting
    
    * test changes
    
    * fix build errors
    
    * add dependencies to Directory.package
    
    * recreated using az func extension for dependencies
    
    * remove webjob references & add worker references.
    
    * add Microsoft.AspNetCore.Mvc.Core ref
    
    * add params func
    
    * add more samples
    
    * fix output bindings and version string
    
    * adding multiple items
    
    * use default deserialization
    
    * create input and output binding folders
    
    * update xml
    
    * remove empty lines
    
    * run package restore and comments
    
    * merge main
    
    * package restore after merging main
    
    * update Mvc.Core to 2.0.1
    
    * remove java classes
    
    * Microsoft.AspNetCore.Mvc.Core to 2.2.0
    
    * remove java changes
    
    * remove mvc core package dependency
    
    * remove sample, simple types supported outofproc
    
    * build error fix

---
+++ samples/samples-outofproc/OutputBindingSamples/AddProductParams.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.WebJobs.Extensions.Sql.SamplesOutOfProc.Common;
+using Microsoft.Azure.Functions.Worker.Extension.Sql;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using System.Web;
+using System.Collections.Specialized;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Sql.SamplesOutOfProc.OutputBindingSamples
+{
+    public static class AddProductParams
+    {
+        [Function("AddProductParams")]
+        [SqlOutput("dbo.Products", ConnectionStringSetting = "SqlConnectionString")]
+        public static Product Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "addproduct-params")]
+            HttpRequestData req)
+        {
+            if (req != null)
+            {
+                NameValueCollection queryStrings = HttpUtility.ParseQueryString(req.Url.Query);
+                var product = new Product()
+                {
+                    Name = queryStrings["name"],
+                    ProductID = int.Parse(queryStrings["productId"], null),
+                    Cost = int.Parse(queryStrings["cost"], null)
+                };
+                return product;
+            }
+            return null;
+        }
+    }
+}
