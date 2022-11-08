f06252ef3c3a856893ca1a4f1c405ea74f99fe2c
Lucy Zhang <luczhan@microsoft.com>
2022-11-08 13:42:55 -0800

    Add java samples (#458)
    * add java samples
    
    * Update samples/samples-java/pom.xml
    
    Co-authored-by: Charles Gagnon <chgagnon@microsoft.com>
    
    * update readme +  add copyright
    
    * update to latest fasterxml
    
    * update to 2.13.4.1
    
    * fix employee.java in readme
    
    Co-authored-by: Charles Gagnon <chgagnon@microsoft.com>

---
+++ samples/samples-java/src/main/java/com/function/AddProduct.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.function;
+
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.OutputBinding;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.microsoft.azure.functions.sql.annotation.SQLOutput;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.function.Common.Product;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class AddProduct {
+    @FunctionName("AddProduct")
+    public HttpResponseMessage run(
+            @HttpTrigger(
+                name = "req",
+                methods = {HttpMethod.POST},
+                authLevel = AuthorizationLevel.ANONYMOUS,
+                route = "addproduct")
+                HttpRequestMessage<Optional<String>> request,
+            @SQLOutput(
+                commandText = "Products",
+                connectionStringSetting = "SqlConnectionString")
+                OutputBinding<Product> product) throws JsonParseException, JsonMappingException, IOException {
+
+        String json = request.getBody().get();
+        ObjectMapper mapper = new ObjectMapper();
+        Product p = mapper.readValue(json, Product.class);
+        product.setValue(p);
+
+        return request.createResponseBuilder(HttpStatus.OK).header("Content-Type", "application/json").body(product).build();
+    }
+}
