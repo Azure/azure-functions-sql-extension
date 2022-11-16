cd3a879e06b4f590d89e498961f6a58087fcfcec
Charles Gagnon <chgagnon@microsoft.com>
2022-11-16 09:13:21 -0800

    Start Azurite once before performance tests (#511)
    * Start azureite once before benchmark tests
    
    * fix

--- performance/SqlBindingBenchmarks.cs
+++ performance/SqlBindingBenchmarks.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System.Diagnostics;
 using System.Linq;
 using BenchmarkDotNet.Running;
 
@@ -12,35 +13,58 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Performance
         {
             bool runAll = args.Length == 0;
 
-            // **IMPORTANT** If changing these make sure to update template-steps-performance.yml as well
-            if (runAll || args.Contains("input"))
+            // Start Azurite once before the tests run - there were issues
+            // with it not getting cleaned up properly between test runs
+            // TODO: This should be a more general thing for all tests
+            var azuriteHost = new Process()
             {
-                BenchmarkRunner.Run<SqlInputBindingPerformance>();
-            }
-            if (runAll || args.Contains("output"))
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "azurite",
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    UseShellExecute = true
+                }
+            };
+            azuriteHost.Start();
+
+            try
             {
-                BenchmarkRunner.Run<SqlOutputBindingPerformance>();
+                // **IMPORTANT** If changing these make sure to update template-steps-performance.yml as well
+                if (runAll || args.Contains("input"))
+                {
+                    BenchmarkRunner.Run<SqlInputBindingPerformance>();
+                }
+                if (runAll || args.Contains("output"))
+                {
+                    BenchmarkRunner.Run<SqlOutputBindingPerformance>();
+                }
+                if (runAll || args.Contains("trigger"))
+                {
+                    BenchmarkRunner.Run<SqlTriggerBindingPerformance>();
+                }
+                if (runAll || args.Contains("trigger_batch"))
+                {
+                    BenchmarkRunner.Run<SqlTriggerBindingPerformance_BatchOverride>();
+                }
+                if (runAll || args.Contains("trigger_poll"))
+                {
+                    BenchmarkRunner.Run<SqlTriggerBindingPerformance_PollingIntervalOverride>();
+                }
+                if (runAll || args.Contains("trigger_overrides"))
+                {
+                    BenchmarkRunner.Run<SqlTriggerPerformance_Overrides>();
+                }
+                if (runAll || args.Contains("trigger_parallel"))
+                {
+                    BenchmarkRunner.Run<SqlTriggerBindingPerformance_Parallelization>();
+                }
             }
-            if (runAll || args.Contains("trigger"))
+            finally
             {
-                BenchmarkRunner.Run<SqlTriggerBindingPerformance>();
+                azuriteHost.Kill();
+                azuriteHost.Dispose();
             }
-            if (runAll || args.Contains("trigger_batch"))
-            {
-                BenchmarkRunner.Run<SqlTriggerBindingPerformance_BatchOverride>();
-            }
-            if (runAll || args.Contains("trigger_poll"))
-            {
-                BenchmarkRunner.Run<SqlTriggerBindingPerformance_PollingIntervalOverride>();
-            }
-            if (runAll || args.Contains("trigger_overrides"))
-            {
-                BenchmarkRunner.Run<SqlTriggerPerformance_Overrides>();
-            }
-            if (runAll || args.Contains("trigger_parallel"))
-            {
-                BenchmarkRunner.Run<SqlTriggerBindingPerformance_Parallelization>();
-            }
+
         }
     }
 }
