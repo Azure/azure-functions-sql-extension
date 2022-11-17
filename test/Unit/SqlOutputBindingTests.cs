f952951ba81da1caf47efab532678ac31abd4ead
Charles Gagnon <chgagnon@microsoft.com>
2022-11-16 16:14:13 -0800

    Remove test

--- test/Unit/SqlOutputBindingTests.cs
+++ test/Unit/SqlOutputBindingTests.cs
@@ -24,23 +24,6 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Unit
             Assert.Throws<ArgumentNullException>(() => new SqlAsyncCollector<string>(null, arg, logger.Object));
         }
 
-        [Fact]
-        public async Task TestAddAsync()
-        {
-            // Really a pretty silly test. Just confirms that the SQL connection is only opened when FlushAsync is called,
-            // because otherwise we would get an exception in AddAsync (since the SQL connection in the wrapper is null)
-            var arg = new SqlAttribute(string.Empty);
-            var collector = new SqlAsyncCollector<TestData>(config.Object, arg, logger.Object);
-            var data = new TestData
-            {
-                ID = 1,
-                Name = "Data",
-                Cost = 10,
-                Timestamp = new DateTime(2019, 11, 22, 6, 32, 15)
-            };
-            await collector.AddAsync(data);
-        }
-
         [Theory]
         [InlineData("dbo.Products", "dbo", "'dbo'", "Products", "'Products'", "dbo.Products", "'[dbo].[Products]'", "[dbo].[Products]")] // Simple full name
         [InlineData("Products", "SCHEMA_NAME()", "SCHEMA_NAME()", "Products", "'Products'", "Products", "'[Products]'", "[Products]")] // Simple no schema
