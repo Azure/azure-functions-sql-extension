cfebd1de192e4a18e2d8563df21c2c2ca77fdb79
Lucy Zhang <luczhan@microsoft.com>
2022-11-22 07:38:05 -0800

    Update scripts/CopySqlDllToExtensionBundle.ps1
    Co-authored-by: Charles Gagnon <chgagnon@microsoft.com>

--- scripts/CopySqlDllToExtensionBundle.ps1
+++ scripts/CopySqlDllToExtensionBundle.ps1
@@ -1,7 +1,10 @@
 # Run this script from the root of the repository to copy the latest
 # SQL extension dll to the extension bundle.
 
-# Run `func GetExtensionBundlePath` from samples-js to get the correct path to the extension bundle.
+# Run `func GetExtensionBundlePath` from samples-js to get the correct path to the extension bundle. 
+# This assumes that the same bundle will be used for all non-.NET sample functions which should 
+# generally be the case, if not run the command above from the same folder of the function you're
+# testing with.
 cd ./samples/samples-js
 $extensionBundlePath = func GetExtensionBundlePath
 $extensionBundleBinPath = Join-Path -Path $extensionBundlePath -ChildPath "bin"
