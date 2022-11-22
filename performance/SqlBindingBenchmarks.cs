b8e65bc456962c582c4ed8409bb9435ce06fcb32
Charles Gagnon <chgagnon@microsoft.com>
2022-11-22 15:23:36 -0800

    Split fixtures

--- performance/SqlBindingBenchmarks.cs
+++ performance/SqlBindingBenchmarks.cs
@@ -13,9 +13,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Performance
         {
             bool runAll = args.Length == 0;
 
-
-
-            using var testFixture = new IntegrationTestFixture(false);
+            using var testFixture = new BaseTestFixture(false);
 
             // **IMPORTANT** If changing these make sure to update template-steps-performance.yml as well
             if (runAll || args.Contains("input"))
