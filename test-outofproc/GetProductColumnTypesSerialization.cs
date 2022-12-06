dda194768ef23194fd24693e34cafc7d2575ae54
Maddy <12754347+MaddyDev@users.noreply.github.com>
2022-12-05 18:34:39 -0800

    Add more tests for OOP (#555)
    * intial commit with tests for samples
    
    * changes for out of proc tests
    
    * remove commented code
    
    * remove tests from worker proj
    
    * initial commit
    
    * exclude outofproc for now
    
    * add local.settings.json
    
    * exclude outofproc for UnsupportedDatabaseThrows
    
    * remove queue trigger sample
    
    * remove OutOfProc from Unsuppported list
    
    * missed a test
    
    * test ColumnType with Date
    
    * Update test-outofproc/host.json
    
    Co-authored-by: Charles Gagnon <chgagnon@microsoft.com>
    
    * fix build error
    
    * exclude AddProductColumnTypesTest for OOP
    
    * add local settings file
    
    * add test-outofproc to sln
    
    * dont overwrite negative tests
    
    * supress warning
    
    * clean up
    
    * enable GetProductsColumnTypesSerializationTest
    
    Co-authored-by: Charles Gagnon <chgagnon@microsoft.com>

---
+++ test-outofproc/GetProductColumnTypesSerialization.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Functions.Worker.Extension.Sql;
+using Microsoft.Azure.Functions.Worker;
+using DotnetIsolatedTests.Common;
+
+namespace DotnetIsolatedTests
+{
+    public static class GetProductsColumnTypesSerialization
+    {
+        /// <summary>
+        /// This function verifies that serializing an item with various data types
+        /// works as expected when using IEnumerable.
+        /// </summary>
+        [Function(nameof(GetProductsColumnTypesSerialization))]
+        public static IEnumerable<ProductColumnTypes> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "getproducts-columntypesserialization")]
+            HttpRequest req,
+            [SqlInput("SELECT * FROM [dbo].[ProductsColumnTypes]",
+                CommandType = System.Data.CommandType.Text,
+                ConnectionStringSetting = "SqlConnectionString")]
+            IEnumerable<ProductColumnTypes> products)
+        {
+            return products;
+        }
+    }
+}
