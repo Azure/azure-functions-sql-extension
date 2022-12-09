283d565195fd91289e7180fe1ff3627c1e4c1933
Maddy Koripalli <makoripa@microsoft.com>
2022-12-09 12:46:27 -0800

    fix build error

--- test-outofproc/AddProductColumnTypes.cs
+++ test-outofproc/AddProductColumnTypes.cs
@@ -26,7 +26,7 @@ namespace DotnetIsolatedTests
             var product = new ProductColumnTypes()
             {
                 ProductID = int.Parse(queryStrings["productId"], null),
-                Datetime = DateTime.Parse(DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffZ", System.Globalization.CultureInfo.InvariantCulture)),
+                Datetime = DateTime.Parse(DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffZ", System.Globalization.CultureInfo.InvariantCulture), System.Globalization.CultureInfo.InvariantCulture),
                 Datetime2 = DateTime.UtcNow
             };
 
