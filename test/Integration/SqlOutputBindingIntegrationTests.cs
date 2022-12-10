552315cdc3b22241fdb43b917717a01d0b85cef6
Vasu Bhog <vabhog@microsoft.com>
2022-12-09 19:11:05 -0800

    remove previous issue from merge

--- test/Integration/SqlOutputBindingIntegrationTests.cs
+++ test/Integration/SqlOutputBindingIntegrationTests.cs
@@ -49,7 +49,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Integration
         [SqlInlineData(-500, "ABCD", 580)]
         // Currently Java functions return null when the parameter for name is an empty string
         // Issue link: https://github.com/Azure/azure-functions-sql-extension/issues/517
-        [UnsupportedLanguages(SupportedLanguages.PowerShell, SupportedLanguages.Java)]
+        [UnsupportedLanguages(SupportedLanguages.Java)]
         public void AddProductParamsTest(int id, string name, int cost, SupportedLanguages lang)
         {
             this.StartFunctionHost(nameof(AddProductParams), lang);
