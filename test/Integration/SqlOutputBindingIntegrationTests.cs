40e734d3cbd640f18011f08d5f33fbcb7c6b890b
Maddy <12754347+MaddyDev@users.noreply.github.com>
2022-12-12 12:01:31 -0800

    convert date to iso 8061 format (#579)
    * convert date to iso 8061 format
    
    * fix build error
    
    * use parse exact
    
    * use SqlDateTime instead
    
    * merge error fix

--- test/Integration/SqlOutputBindingIntegrationTests.cs
+++ test/Integration/SqlOutputBindingIntegrationTests.cs
@@ -114,8 +114,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Integration
         /// <param name="lang">The language to run the test against</param>
         [Theory]
         [SqlInlineData()]
-        // Java issue: https://github.com/Azure/azure-functions-sql-extension/issues/521
-        [UnsupportedLanguages(SupportedLanguages.Java, SupportedLanguages.PowerShell, SupportedLanguages.OutOfProc)]
+        [UnsupportedLanguages(SupportedLanguages.Java, SupportedLanguages.PowerShell)]
         public void AddProductColumnTypesTest(SupportedLanguages lang)
         {
             this.StartFunctionHost(nameof(AddProductColumnTypes), lang, true);
