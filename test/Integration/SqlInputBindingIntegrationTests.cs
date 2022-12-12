b534aa4295c8b668cf20c4892c76904f0a344ae9
Lucy Zhang <luczhan@microsoft.com>
2022-12-12 07:02:22 -0800

    enable getcolumntypes test for java

--- test/Integration/SqlInputBindingIntegrationTests.cs
+++ test/Integration/SqlInputBindingIntegrationTests.cs
@@ -161,9 +161,6 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Integration
         /// </summary>
         [Theory]
         [SqlInlineData()]
-        // Java worker returns timestamps in local time zone
-        // https://github.com/Azure/azure-functions-sql-extension/issues/515
-        [UnsupportedLanguages(SupportedLanguages.Java)]
         public async void GetProductsColumnTypesSerializationTest(SupportedLanguages lang)
         {
             this.StartFunctionHost(nameof(GetProductsColumnTypesSerialization), lang, true);
