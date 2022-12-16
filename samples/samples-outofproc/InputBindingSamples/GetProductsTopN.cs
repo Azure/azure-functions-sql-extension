b18de323e8f3684be7155ea61f15e830d068f8f7
Maddy <12754347+MaddyDev@users.noreply.github.com>
2022-12-16 14:16:18 -0800

    rename package to Worker.Extensions.Sql (#590)
    * rename package to Worker.Extensions.Sql
    
    * remove connection strings
    
    * rename folder
    
    * fix path

--- samples/samples-outofproc/InputBindingSamples/GetProductsTopN.cs
+++ samples/samples-outofproc/InputBindingSamples/GetProductsTopN.cs
@@ -4,7 +4,7 @@
 using System.Collections.Generic;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Azure.WebJobs.Extensions.Sql.SamplesOutOfProc.Common;
-using Microsoft.Azure.Functions.Worker.Extension.Sql;
+using Microsoft.Azure.Functions.Worker.Extensions.Sql;
 using Microsoft.Azure.Functions.Worker;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Sql.SamplesOutOfProc.InputBindingSamples
