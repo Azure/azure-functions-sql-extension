73893ae41c5cd6b6db3d116a6351861146b47537
Lucy Zhang <luczhan@microsoft.com>
2022-12-16 08:42:36 -0800

    addproductextracolumns test

---
+++ test/Integration/test-python/AddProductExtraColumns/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import azure.functions as func
+from Common.productextracolumns import ProductExtraColumns
+
+# This output binding should throw an Exception because the ProductExtraColumns object has
+# two properties that do not exist as columns in the SQL table (ExtraInt and ExtraString).
+def main(req: func.HttpRequest, product: func.Out[func.SqlRow]) -> func.HttpResponse:
+    productExtraColumns = func.SqlRow(productExtraColumns(1, "test", 100, 1, "test"))
+    product.set(productExtraColumns)
+
+    return func.HttpResponse(
+        body=req.get_body(),
+        status_code=201,
+        mimetype="application/json"
+    )
