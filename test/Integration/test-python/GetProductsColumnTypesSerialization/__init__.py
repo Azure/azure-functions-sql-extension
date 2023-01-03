bd53c0a6d599c60ea3baed8e7b1a1f68326b93e2
Lucy Zhang <luczhan@microsoft.com>
2023-01-03 09:45:40 -0800

    GetProductsColumnTypesSerializationTest

--- test/Integration/test-python/GetProductsColumnTypesSerialization/__init__.py
+++ test/Integration/test-python/GetProductsColumnTypesSerialization/__init__.py
@@ -2,17 +2,16 @@
 # Licensed under the MIT License.
 
 import json
-import logging
 
 import azure.functions as func
 
 # This function verifies that serializing an item with various data types
 # works as expected.
 def main(req: func.HttpRequest, products: func.SqlRowList) -> func.HttpResponse:
-    logging.info(json.dumps(products.__dict__, default=lambda o: o.to_json()))
+    rows = list(map(lambda r: json.loads(r.to_json()), products))
 
     return func.HttpResponse(
-        body=json.dumps(products.__dict__, default=lambda o: o.to_json()),
+        body=json.dumps(rows),
         status_code=200,
         mimetype="application/json"
     )
