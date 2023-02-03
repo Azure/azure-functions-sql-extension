5bd84b6ff4db544687b65ea8bdc667f7105368a8
maddydev <makoripa@microsoft.com>
2023-02-03 12:15:25 -0800

    add workerunittest class

---
+++ test-outofproc/Unit/WorkerUnitTests.cs
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.Functions.Worker.Extensions.Sql;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+using System;
+
+namespace WorkerUnitTests
+{
+    public class SqlInputBindingTests
+    {
+        [Fact]
+        public void TestNullCommandText()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SqlInputAttribute(null));
+            Assert.Throws<ArgumentNullException>(() => new SqlOutputAttribute(null));
+
+        }
+
+        [Fact]
+        public void TestNullBuilder()
+        {
+            HostBuilder builder = null;
+            Assert.Throws<ArgumentNullException>(() => builder.ConfigureFunctionsWorkerDefaults());
+        }
+
+    }
+}
