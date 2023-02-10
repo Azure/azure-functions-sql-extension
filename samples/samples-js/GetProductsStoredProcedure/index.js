e2c1d03a646ab9d0b065737b4eb71ee811a8e1b6
Maddy <12754347+MaddyDev@users.noreply.github.com>
2023-02-10 15:12:17 -0800

    Update SetupGuide_Javascript (#674)
    * add js GetProductsNameNull sample & complete docs
    
    * update toc
    
    * move sample description to sample files.

--- samples/samples-js/GetProductsStoredProcedure/index.js
+++ samples/samples-js/GetProductsStoredProcedure/index.js
@@ -1,6 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+// `SelectsProductCost` is the name of a procedure stored in the user's database.
+// In this case, *CommandType* is `StoredProcedure`. The parameter value of the `@Cost` parameter in the
+// procedure is once again the `{cost}` specified in the `getproducts-storedprocedure/{cost}` URL.
 module.exports = async function (context, req, products) {
     context.log('JavaScript HTTP trigger function processed a request.');
     context.log(JSON.stringify(products));
