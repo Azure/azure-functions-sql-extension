a8bc45bfd7a96f8f7c4f0b64add075f8538bbdb4
Maddy Koripalli <makoripa@microsoft.com>
2023-03-13 12:42:26 -0700

    add ReleaseType parameter

--- samples/samples-csharp/OutputBindingSamples/TimerTriggerProducts.cs
+++ samples/samples-csharp/OutputBindingSamples/TimerTriggerProducts.cs
@@ -1,44 +1,44 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using Microsoft.Azure.WebJobs.Extensions.Sql.Samples.Common;
-using Microsoft.Extensions.Logging;
+// using System;
+// using System.Collections.Generic;
+// using System.Diagnostics;
+// using Microsoft.Azure.WebJobs.Extensions.Sql.Samples.Common;
+//using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Sql.Samples.OutputBindingSamples
 {
     public static class TimerTriggerProducts
     {
-        private static int _executionNumber = 0;
+        //private static int _executionNumber = 0;
 
         /// <summary>
         /// This timer function runs every 5 seconds, each time it upserts 1000 rows of data.
         /// </summary>
-        [FunctionName("TimerTriggerProducts")]
-        public static void Run(
-            [TimerTrigger("*/5 * * * * *")] TimerInfo req, ILogger log,
-            [Sql("Products", "SqlConnectionString")] ICollector<Product> products)
-        {
-            int totalUpserts = 1000;
-            log.LogInformation($"{DateTime.Now} starting execution #{_executionNumber}. Rows to generate={totalUpserts}.");
+        // [FunctionName("TimerTriggerProducts")]
+        // public static void Run(
+        //     [TimerTrigger("*/5 * * * * *")] TimerInfo req, ILogger log,
+        //     [Sql("Products", "SqlConnectionString")] ICollector<Product> products)
+        // {
+        //     int totalUpserts = 1000;
+        //     log.LogInformation($"{DateTime.Now} starting execution #{_executionNumber}. Rows to generate={totalUpserts}.");
 
-            var sw = new Stopwatch();
-            sw.Start();
+        //     var sw = new Stopwatch();
+        //     sw.Start();
 
-            List<Product> newProducts = ProductUtilities.GetNewProducts(totalUpserts);
-            foreach (Product product in newProducts)
-            {
-                products.Add(product);
-            }
+        //     List<Product> newProducts = ProductUtilities.GetNewProducts(totalUpserts);
+        //     foreach (Product product in newProducts)
+        //     {
+        //         products.Add(product);
+        //     }
 
-            sw.Stop();
+        //     sw.Stop();
 
-            string line = $"{DateTime.Now} finished execution #{_executionNumber}. Total time to create {totalUpserts} rows={sw.ElapsedMilliseconds}.";
-            log.LogInformation(line);
+        //     string line = $"{DateTime.Now} finished execution #{_executionNumber}. Total time to create {totalUpserts} rows={sw.ElapsedMilliseconds}.";
+        //     log.LogInformation(line);
 
-            _executionNumber++;
-        }
+        //     _executionNumber++;
+        // }
     }
 }
--- src/SqlBindingUtilities.cs
+++ src/SqlBindingUtilities.cs
@@ -45,7 +45,13 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
             {
                 throw new ArgumentNullException(nameof(configuration));
             }
-            return configuration.GetConnectionStringOrSetting(connectionStringSetting);
+            string connectionString = configuration.GetConnectionStringOrSetting(connectionStringSetting);
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw connectionString == null ? new ArgumentException($"ConnectionStringSetting '{connectionStringSetting}' is missing in your local settings, Please add the setting which " +
+                    "contains a SQL connection string") : new ArgumentException($"ConnectionStringSetting '{connectionStringSetting}' must not be empty, it should contain a valid SQL connection string for connection.");
+            }
+            return connectionString;
         }
 
         /// <summary>
