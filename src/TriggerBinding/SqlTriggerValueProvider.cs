34153649e898765ce6f1888926acf9ff2f839e24
Lucy Zhang <luczhan@microsoft.com>
2023-03-15 13:01:35 -0700

    Add JObject support for SQL trigger (#722)
    * add support for jobject and js, ps, python samples
    
    * use utils JsonSerializeObject + comment
    
    * Update src/TriggerBinding/SqlTriggerBindingProvider.cs
    
    Co-authored-by: Charles Gagnon <chgagnon@microsoft.com>
    
    * fix unit test + pylint
    
    ---------
    
    Co-authored-by: Charles Gagnon <chgagnon@microsoft.com>

--- src/TriggerBinding/SqlTriggerValueProvider.cs
+++ src/TriggerBinding/SqlTriggerValueProvider.cs
@@ -2,9 +2,10 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs.Host.Bindings;
-
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Sql
 {
@@ -15,6 +16,8 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
     {
         private readonly object _value;
         private readonly string _tableName;
+        private readonly Type _parameterType;
+        private readonly bool _isString;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SqlTriggerValueProvider"/> class.
@@ -24,21 +27,37 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
         /// <param name="tableName">Name of the user table</param>
         public SqlTriggerValueProvider(Type parameterType, object value, string tableName)
         {
-            this.Type = parameterType;
+            this._parameterType = parameterType;
             this._value = value;
             this._tableName = tableName;
+            this._isString = parameterType == typeof(string);
         }
 
         /// <summary>
         /// Gets the trigger argument value.
         /// </summary>
-        public Type Type { get; }
+        public Type Type
+        {
+            get
+            {
+                if (this._isString)
+                {
+                    return typeof(IReadOnlyCollection<JObject>);
+                }
+                return this._parameterType;
+            }
+        }
 
         /// <summary>
         /// Returns value of the trigger argument.
         /// </summary>
         public Task<object> GetValueAsync()
         {
+            if (this._isString)
+            {
+                return Task.FromResult<object>(Utils.JsonSerializeObject(this._value));
+            }
+
             return Task.FromResult(this._value);
         }
 
