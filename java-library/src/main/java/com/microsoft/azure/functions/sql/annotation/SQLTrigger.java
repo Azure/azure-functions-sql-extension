8a8e2e41c5e543e2b77e3e8129bdf98a12c9a2d1
Lucy Zhang <luczhan@microsoft.com>
2023-04-04 15:01:58 -0700

    Add SQLTrigger annotation to java library (#783)
    * add SQLTrigger annotation to java library
    
    * remove default values
    
    * use enum for commandtype

--- java-library/src/main/java/com/microsoft/azure/functions/sql/annotation/SQLTrigger.java
+++ java-library/src/main/java/com/microsoft/azure/functions/sql/annotation/SQLTrigger.java
@@ -15,32 +15,20 @@ import com.microsoft.azure.functions.annotation.CustomBinding;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
-@CustomBinding(direction = "in", name = "", type = "sql")
-public @interface SQLInput {
+@CustomBinding(direction = "in", name = "", type = "sqlTrigger")
+public @interface SQLTrigger {
     /**
      * The variable name used in function.json.
-     */
+    */
     String name();
 
     /**
-     * Query string or name of stored procedure to be run.
-     */
-    String commandText();
-
-    /**
-     * Text or Stored Procedure.
-     */
-    CommandType commandType();
-
-    /**
-     * Parameters to the query or stored procedure. This string must follow the format
-     * "@param1=param1,@param2=param2" where @param1 is the name of the parameter and
-     * param1 is the parameter value.
-     */
-    String parameters() default "";
+     * Name of the table to watch for changes.
+    */
+    String tableName();
 
     /**
      * Setting name for SQL connection string.
-     */
+    */
     String connectionStringSetting();
 }
