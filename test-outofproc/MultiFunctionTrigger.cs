b56fff0140feb0a619bdeb81f81c79fd3eb55bfe
Maddy <12754347+MaddyDev@users.noreply.github.com>
2023-04-12 08:09:17 -0700

    enable rest of the trigger tests for oop (#798)
    * enable all trigger tests for OOP
    
    * undo
    
    * fix test
    
    * merge conflict
    
    * remove Java

--- test-outofproc/MultiFunctionTrigger.cs
+++ test-outofproc/MultiFunctionTrigger.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using DotnetIsolatedTests.Common;
 using Microsoft.Extensions.Logging;
@@ -15,22 +16,24 @@ namespace DotnetIsolatedTests
     /// </summary>
     public static class MultiFunctionTrigger
     {
+        private static readonly Action<ILogger, string, Exception> _loggerMessage = LoggerMessage.Define<string>(LogLevel.Information, eventId: new EventId(0, "INFO"), formatString: "{Message}");
+
         [Function(nameof(MultiFunctionTrigger1))]
         public static void MultiFunctionTrigger1(
             [SqlTrigger("[dbo].[Products]", "SqlConnectionString")]
             IReadOnlyList<SqlChange<Product>> products,
-            ILogger logger)
+            FunctionContext context)
         {
-            logger.LogInformation("Trigger1 Changes: " + Utils.JsonSerializeObject(products));
+            _loggerMessage(context.GetLogger("ProductsTriggerWithValidation"), "Trigger1 Changes: " + Utils.JsonSerializeObject(products), null);
         }
 
         [Function(nameof(MultiFunctionTrigger2))]
         public static void MultiFunctionTrigger2(
             [SqlTrigger("[dbo].[Products]", "SqlConnectionString")]
             IReadOnlyList<SqlChange<Product>> products,
-            ILogger logger)
+            FunctionContext context)
         {
-            logger.LogInformation("Trigger2 Changes: " + Utils.JsonSerializeObject(products));
+            _loggerMessage(context.GetLogger("ProductsTriggerWithValidation"), "Trigger2 Changes: " + Utils.JsonSerializeObject(products), null);
         }
     }
 }
