683d4210c5199867cc87773b5300c3d6bf0cf741
Charles Gagnon <chgagnon@microsoft.com>
2023-04-18 13:33:09 -0700

    Cleanup trigger logging (#808)
    * Cleanup trigger logging
    
    * more
    
    * fix compile
    
    * Default log level of debug
    
    * Fix scale monitor tests

--- src/TriggerBinding/SqlTriggerTargetScaler.cs
+++ src/TriggerBinding/SqlTriggerTargetScaler.cs
@@ -12,17 +12,11 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
     /// </summary>
     internal sealed class SqlTriggerTargetScaler : ITargetScaler
     {
-        private readonly string _userFunctionId;
-        private readonly string _userTableName;
-        private readonly ILogger _logger;
         private readonly SqlTriggerMetricsProvider _metricsProvider;
         private readonly int _maxChangesPerWorker;
 
         public SqlTriggerTargetScaler(string userFunctionId, string userTableName, string connectionString, int maxChangesPerWorker, ILogger logger)
         {
-            this._userTableName = !string.IsNullOrEmpty(userTableName) ? userTableName : throw new ArgumentNullException(userTableName);
-            this._userFunctionId = !string.IsNullOrEmpty(userFunctionId) ? userFunctionId : throw new ArgumentNullException(userFunctionId);
-            this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
             this._metricsProvider = new SqlTriggerMetricsProvider(connectionString, logger, new SqlObject(userTableName), userFunctionId);
             this.TargetScalerDescriptor = new TargetScalerDescriptor(userFunctionId);
             this._maxChangesPerWorker = maxChangesPerWorker;
@@ -37,10 +31,10 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
             // Instance concurrency value is set by the functions host when dynamic concurrency is enabled. See https://learn.microsoft.com/en-us/azure/azure-functions/functions-concurrency for more details.
             int concurrency = context.InstanceConcurrency ?? this._maxChangesPerWorker;
 
-            return this.GetScaleResultInternal(concurrency, metrics.UnprocessedChangeCount);
+            return GetScaleResultInternal(concurrency, metrics.UnprocessedChangeCount);
         }
 
-        internal TargetScalerResult GetScaleResultInternal(int concurrency, long unprocessedChangeCount)
+        internal static TargetScalerResult GetScaleResultInternal(int concurrency, long unprocessedChangeCount)
         {
             if (concurrency < 1)
             {
@@ -48,8 +42,6 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
             }
 
             int targetWorkerCount = (int)Math.Ceiling(unprocessedChangeCount / (decimal)concurrency);
-
-            this._logger.LogInformation($"Target worker count for function '{this._userFunctionId}' is '{targetWorkerCount}' TableName ='{this._userTableName}', UnprocessedChangeCount ='{unprocessedChangeCount}', Concurrency='{concurrency}').");
 
             return new TargetScalerResult
             {
