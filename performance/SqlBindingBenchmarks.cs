8e6519be2193c0e50e259d19365bf1535165f4cd
Lucy Zhang <luczhan@microsoft.com>
2023-05-04 07:06:16 -0700

    finally block

--- performance/SqlBindingBenchmarks.cs
+++ performance/SqlBindingBenchmarks.cs
@@ -16,43 +16,48 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Performance
 
             Process azuriteHost = TestUtils.StartAzurite();
             TestUtils.SetupDatabase(out string masterConnectionString, out string connectionString);
+            try
+            {
+                // **IMPORTANT** If changing these make sure to update template-steps-performance.yml as well
+                if (runAll || args.Contains("input"))
+                {
+                    BenchmarkRunner.Run<SqlInputBindingPerformance>();
+                }
+                if (runAll || args.Contains("output"))
+                {
+                    BenchmarkRunner.Run<SqlOutputBindingPerformance>();
+                }
+                if (runAll || args.Contains("trigger"))
+                {
+                    BenchmarkRunner.Run<SqlTriggerBindingPerformance>();
+                }
+                if (runAll || args.Contains("trigger_batch"))
+                {
+                    BenchmarkRunner.Run<SqlTriggerBindingPerformance_BatchOverride>();
+                }
+                if (runAll || args.Contains("trigger_poll"))
+                {
+                    BenchmarkRunner.Run<SqlTriggerBindingPerformance_PollingIntervalOverride>();
+                }
+                if (runAll || args.Contains("trigger_overrides"))
+                {
+                    BenchmarkRunner.Run<SqlTriggerPerformance_Overrides>();
+                }
+                if (runAll || args.Contains("trigger_parallel"))
+                {
+                    BenchmarkRunner.Run<SqlTriggerBindingPerformance_Parallelization>();
+                }
+                if (runAll || args.Contains("trigger_changerate"))
+                {
+                    BenchmarkRunner.Run<SqlTriggerBindingPerformance_ChangeRate>();
+                }
+            }
+            finally
+            {
+                TestUtils.StopAzurite(azuriteHost);
+                TestUtils.DropDatabase(masterConnectionString, connectionString);
 
-            // **IMPORTANT** If changing these make sure to update template-steps-performance.yml as well
-            if (runAll || args.Contains("input"))
-            {
-                BenchmarkRunner.Run<SqlInputBindingPerformance>();
             }
-            if (runAll || args.Contains("output"))
-            {
-                BenchmarkRunner.Run<SqlOutputBindingPerformance>();
-            }
-            if (runAll || args.Contains("trigger"))
-            {
-                BenchmarkRunner.Run<SqlTriggerBindingPerformance>();
-            }
-            if (runAll || args.Contains("trigger_batch"))
-            {
-                BenchmarkRunner.Run<SqlTriggerBindingPerformance_BatchOverride>();
-            }
-            if (runAll || args.Contains("trigger_poll"))
-            {
-                BenchmarkRunner.Run<SqlTriggerBindingPerformance_PollingIntervalOverride>();
-            }
-            if (runAll || args.Contains("trigger_overrides"))
-            {
-                BenchmarkRunner.Run<SqlTriggerPerformance_Overrides>();
-            }
-            if (runAll || args.Contains("trigger_parallel"))
-            {
-                BenchmarkRunner.Run<SqlTriggerBindingPerformance_Parallelization>();
-            }
-            if (runAll || args.Contains("trigger_changerate"))
-            {
-                BenchmarkRunner.Run<SqlTriggerBindingPerformance_ChangeRate>();
-            }
-
-            TestUtils.StopAzurite(azuriteHost);
-            TestUtils.DropDatabase(masterConnectionString, connectionString);
         }
     }
 }
