e0826728b87f45dd0d6ea20004d9d297bc4131d1
Lucy Zhang <luczhan@microsoft.com>
2023-06-20 12:20:49 -0700

    try running js

--- test/Integration/SqlTriggerBindingIntegrationTests.cs
+++ test/Integration/SqlTriggerBindingIntegrationTests.cs
@@ -661,7 +661,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Integration
         /// </summary>
         [Theory]
         [SqlInlineData()]
-        [UnsupportedLanguages(SupportedLanguages.JavaScript, SupportedLanguages.Python, SupportedLanguages.PowerShell, SupportedLanguages.Csx, SupportedLanguages.Java, SupportedLanguages.OutOfProc)]
+        [UnsupportedLanguages(SupportedLanguages.CSharp, SupportedLanguages.Python, SupportedLanguages.PowerShell, SupportedLanguages.Csx, SupportedLanguages.Java, SupportedLanguages.OutOfProc)]
         public async Task ProductsColumnTypesTriggerTest(SupportedLanguages lang)
         {
             this.SetChangeTrackingForTable("ProductsColumnTypes");
