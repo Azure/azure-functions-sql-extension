3c4a5cd44c407786785c46af9c0ff9f34394cecb
chlafreniere <hichise@gmail.com>
2023-07-14 14:33:04 -0700

    Change name to match function host

--- test/Integration/SqlTriggerBindingIntegrationTests.cs
+++ test/Integration/SqlTriggerBindingIntegrationTests.cs
@@ -661,8 +661,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Integration
         /// </summary>
         [Theory]
         [SqlInlineData()]
-        [UnsupportedLanguages(SupportedLanguages.JavaScript, SupportedLanguages.Python, SupportedLanguages.PowerShell, SupportedLanguages.Csx, SupportedLanguages.Java, SupportedLanguages.CSharp)]
-        public async void ReservedTableNameTest(SupportedLanguages lang)
+        public async void ReservedTableNameTriggerTest(SupportedLanguages lang)
         {
             this.SetChangeTrackingForTable("User");
             this.StartFunctionHost(nameof(ReservedTableNameTrigger), lang, true);
