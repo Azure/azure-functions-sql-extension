f5411053a2452caec50314ae18ad553a4380b817
Maddy Koripalli <makoripa@microsoft.com>
2023-07-20 11:13:17 -0700

    undo exception change

--- test/Common/SupportedLanguagesTestAttribute.cs
+++ test/Common/SupportedLanguagesTestAttribute.cs
@@ -46,7 +46,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Common
         public override IEnumerable<object[]> GetData(MethodInfo testMethod)
         {
             if (testMethod == null) { throw new ArgumentNullException(nameof(testMethod)); }
-            if (this.testData.Count == 0) { throw new ArgumentNullException(nameof(testMethod)); }
+            if (this.testData.Count == 0) { throw new EmptyException(nameof(testMethod)); }
 
             UnsupportedLanguagesAttribute unsupportedLangAttr = testMethod.GetCustomAttribute<UnsupportedLanguagesAttribute>();
             if (unsupportedLangAttr != null)
