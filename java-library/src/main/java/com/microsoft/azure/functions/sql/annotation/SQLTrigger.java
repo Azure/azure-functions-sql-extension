75f8a0fef7143391025342097423ac27c0627020
Lucy Zhang <luczhan@microsoft.com>
2023-08-01 07:33:48 -0700

    add leasestablename to java sqltrigger

--- java-library/src/main/java/com/microsoft/azure/functions/sql/annotation/SQLTrigger.java
+++ java-library/src/main/java/com/microsoft/azure/functions/sql/annotation/SQLTrigger.java
@@ -31,4 +31,11 @@ public @interface SQLTrigger {
      * Setting name for SQL connection string.
     */
     String connectionStringSetting();
+
+    /**
+     * Optional. Name of the table used to store leases. If not specified, the leases table name will be
+     * Leases_{FunctionId}_{TableId}. More information on how this is generated can be found here:
+     * https://github.com/Azure/azure-functions-sql-extension/blob/release/trigger/docs/TriggerBinding.md#az_funcleases_).
+     */
+    String leasesTableName() default "";
 }
