7c51e7a5425b890d285c23bf08648f4dd3472f9e
Maddy Koripalli <makoripa@microsoft.com>
2023-08-23 12:13:40 -0700

    remove retrytheory

--- test/Integration/SqlTriggerBindingIntegrationTests.cs
+++ test/Integration/SqlTriggerBindingIntegrationTests.cs
@@ -579,7 +579,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql.Tests.Integration
         /// <summary>
         /// Tests that when a user function throws an exception we'll retry executing that function once the lease timeout expires
         /// </summary>
-        [RetryTheory]
+        [Theory]
         [SqlInlineData()]
         [UnsupportedLanguages(SupportedLanguages.JavaScript, SupportedLanguages.Python, SupportedLanguages.PowerShell, SupportedLanguages.Csx, SupportedLanguages.Java, SupportedLanguages.OutOfProc)] // Keeping static state for threwException across calls is only valid for C# and Java.
         public async Task FunctionExceptionsCauseRetry(SupportedLanguages lang)
