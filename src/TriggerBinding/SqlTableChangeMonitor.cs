33336f20e49b8a89c9ec7221bf4965f50c4752c8
Charles Gagnon <chgagnon@microsoft.com>
2023-11-27 15:53:17 -0800

    Let fatal exceptions when getting locked row count bubble up

--- src/TriggerBinding/SqlTableChangeMonitor.cs
+++ src/TriggerBinding/SqlTableChangeMonitor.cs
@@ -860,8 +860,17 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
             {
                 this._logger.LogError($"Failed to query count of lease locked changes for table '{this._userTable.FullName}' due to exception: {ex.GetType()}. Exception message: {ex.Message}");
                 TelemetryInstance.TrackException(TelemetryErrorName.GetLeaseLockedRowCount, ex, null, new Dictionary<TelemetryMeasureName, double>() { { TelemetryMeasureName.GetLockedRowCountDurationMs, getLockedRowCountDurationMs } });
-                // This is currently only used for debugging, so return a -1 instead of throwing since it isn't necessary to get the value
-                leaseLockedRowsCount = -1;
+                // This is currently only used for debugging, so ignore the exception if we can. If the error is a fatal one though then the connection will be
+                // closed so we have to let this bubble up so we can attempt to reconnect
+                if (ex.IsFatalSqlException() || connection.IsBrokenOrClosed())
+                {
+                    throw;
+                }
+                else
+                {
+                    // If it's non-fatal though return a -1 instead of throwing since it isn't necessary to get the value
+                    leaseLockedRowsCount = -1;
+                }
             }
             return leaseLockedRowsCount;
         }
