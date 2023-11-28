2f4bab9643c4081de69b54bb804c48cfe6958d2e
Charles Gagnon <chgagnon@microsoft.com>
2023-11-27 16:27:50 -0800

    Check for deadlock

--- src/TriggerBinding/SqlTableChangeMonitor.cs
+++ src/TriggerBinding/SqlTableChangeMonitor.cs
@@ -242,6 +242,10 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
                             this._logger.LogError($"Fatal SQL Client exception processing changes. Will attempt to reestablish connection in {this._pollingIntervalInMs}ms. Exception = {e.Message}");
                             forceReconnect = true;
                         }
+                        catch (Exception e) when (e.IsDeadlockException())
+                        {
+                            // Deadlocks aren't fatal and don't need a reconnection so just let the loop try again after the normal delay
+                        }
                         await Task.Delay(TimeSpan.FromMilliseconds(this._pollingIntervalInMs), token);
                     }
                 }
@@ -860,9 +864,9 @@ namespace Microsoft.Azure.WebJobs.Extensions.Sql
             {
                 this._logger.LogError($"Failed to query count of lease locked changes for table '{this._userTable.FullName}' due to exception: {ex.GetType()}. Exception message: {ex.Message}");
                 TelemetryInstance.TrackException(TelemetryErrorName.GetLeaseLockedRowCount, ex, null, new Dictionary<TelemetryMeasureName, double>() { { TelemetryMeasureName.GetLockedRowCountDurationMs, getLockedRowCountDurationMs } });
-                // This is currently only used for debugging, so ignore the exception if we can. If the error is a fatal one though then the connection will be
-                // closed so we have to let this bubble up so we can attempt to reconnect
-                if (ex.IsFatalSqlException() || connection.IsBrokenOrClosed())
+                // This is currently only used for debugging, so ignore the exception if we can. If the error is a fatal one though then the connection or transaction will be
+                // unusable so we have to let this bubble up so we can attempt to reconnect
+                if (ex.IsFatalSqlException() || ex.IsDeadlockException() || connection.IsBrokenOrClosed())
                 {
                     throw;
                 }
