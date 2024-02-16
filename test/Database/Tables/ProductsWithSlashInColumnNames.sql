7d55a8c06bc0d36615ea953cc6eaed256c2e9418
Maddy <12754347+MaddyDev@users.noreply.github.com>
2024-02-16 13:57:41 -0800

    Fix output binding when using column names with slashes (#1042)
    * replace the / and \ slash while building the query
    
    * filter the test for .net inproc tests
    
    * update test for back slash
    
    * update test
    
    * fix

---
+++ test/Database/Tables/ProductsWithSlashInColumnNames.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS [ProductsWithSlashInColumnNames];
+
+CREATE TABLE [ProductsWithSlashInColumnNames] (
+    [ProductId] [int] NOT NULL PRIMARY KEY,
+    [Name/Test] [nchar](100) NOT NULL,
+    [Cost\Test] [int] NOT NULL
+)
---
+++ test/Integration/test-js/AddProductWithSlashInColumnName/index.js
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+// This output binding should successfully add the productMissingColumns object
+// to the SQL table.
+module.exports = async function (context, req) {
+    const productWithSlashInColumnName = req.body ?? [{
+        "ProductId": 1,
+        "Name/Test": "Test",
+        "Cost\\Test": 1
+    }];
+
+    context.bindings.products = JSON.stringify(productWithSlashInColumnName);
+
+    return {
+        status: 201,
+        body: productWithSlashInColumnName
+    };
+}
